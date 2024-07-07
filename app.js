ea657b60887095eca498f9a90b496796859f8945
Zidane <mzidane49@gmail.com>
2024-07-07 14:27:26 +0700

    first commit 5

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ app.get('/logs/:userId', async (req, res) => {
 
     try {
         // Fetch logs by userId
-        const sql = 'SELECT * FROM log WHERE id = ?';
+        const sql = 'SELECT * FROM Logs WHERE id = ?';
         const params = [userId];
         const logs = await executeQuery(sql, params);
 
